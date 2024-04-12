4611666201da47ca6765580a4cd87691e95ed6e8
DanielMascarenhas <54721118+DanielMascarenhas@users.noreply.github.com>
2024-04-12 19:53:37 -0300

    Delete Eclipse directory

--- Eclipse/Interface/src/Atividade/Conta_bancaria.java
+++
@@ -1,7 +0,0 @@
-package Atividade;
-
-public interface Conta_bancaria {
-	public abstract void sacar(double saldo);
-	public abstract void depositar(double saldo);
-	public abstract void rendimento();
-}
